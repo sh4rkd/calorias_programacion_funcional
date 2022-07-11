df84179e62c1f1f4bd3cf5bf950f7d50de71c6cc
Sh4rkD <20006132@es.uveg.edu.mx>
2022-07-10 20:51:07 -0600

    agegando elementos a un array y limpiando campos

--- js/main.js
+++ js/main.js
@@ -6,6 +6,34 @@ let calories = $('#calories')
 let carbs = $('#carbs')
 let protein = $('#protein')
 
+let list = [
+  {
+    description: 'Manzana',
+    calories: 10,
+    carbs: 10,
+    protein: 10
+  },
+  {
+    description: 'Pera',
+    calories: 10,
+    carbs: 10,
+    protein: 10
+  },
+  {
+    description: 'Banana',
+    calories: 10,
+    carbs: 10,
+    protein: 10
+  },
+  {
+    description: 'Naranja',
+    calories: 10,
+    carbs: 10,
+    protein: 10
+  }
+]
+
+
 description.keypress(() => {
   description.removeClass('is-invalid')
 })
@@ -34,5 +62,26 @@ const validateInputs = () => {
     calories.val() &&
     carbs.val() &&
     protein.val()
-  ) console.log('Agregar item a lista')
+  ) {
+    add(list)
+    cleanInputs()
+  }
+}
+
+const add = (list) => {
+  const newItem = {
+    description: description.val(),
+    calories: parseInt(calories.val()),
+    carbs: parseInt(carbs.val()),
+    protein: parseInt(protein.val())
+  }
+  list.push(newItem)
+  console.log(list)
+}
+
+const cleanInputs = () => {
+  description.val('')
+  calories.val('')
+  carbs.val('')
+  protein.val('')
 }
